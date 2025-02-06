42bf5555cf2e019d086e3bd980da09f1cc37ca2c
LKA102 <stefano.hernandez@unmsm.edu.pe>
2025-02-06 17:27:06 -0500

    Add login screen for students and recruiters

--- flutter_app/lib/widgets/login_form.dart
+++ flutter_app/lib/widgets/login_form.dart
@@ -5,13 +5,13 @@ class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
 
   @override
-  _LoginFormState createState() => _LoginFormState();
+  State<StatefulWidget> createState() => _LoginFormState();
 }
 
 class _LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
+  late TextEditingController _email;
+  late TextEditingController _password;
 
   void _submit() {
     if (_formKey.currentState!.validate()) {
@@ -25,89 +25,96 @@ class _LoginFormState extends State<LoginForm> {
   }
 
   @override
+  void initState() {
+    _email = TextEditingController(text: "");
+    _password = TextEditingController(text: "");
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            width: 275.39,
-            height: 75,
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Código admin',
-                filled: true,
-                fillColor: Color(0xFFDBE2F6),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF1E3984), width: 1.0),
-                  borderRadius: BorderRadius.circular(6.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF1E3984), width: 1.0),
-                ),
-              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, ingrese su email';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _email = value!;
-              },
+    return Container(
+      padding: const EdgeInsets.all(30),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/logo_azul.png',
+              height: 208,
+              width: 211,
             ),
-          ),
-          SizedBox(height: 7),
-          SizedBox(
-            width: 275.39,
-            height: 75,
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Contraseña admin',
-                filled: true,
-                fillColor: Color(0xFFDBE2F6),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF1E3984), width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF1E3984), width: 1.0),
-                ),
-              ),
+            TextFormField(
+              controller: _email,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Ej: pepito@unmsm.edu.pe',
+                label: Text("Correo electrónico o telefono")
+              )
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _password,
               obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, ingrese su contraseña';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _password = value!;
-              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Ej: 123456',
+                label: Text("Contraseña")
+              )
             ),
-          ),
-          SizedBox(height: 7),
-          SizedBox(
-            width: 150,
-            height: 48,
-            child: ElevatedButton(
-              onPressed: _submit,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF1E3984),
-                foregroundColor: Color(0xFFF5F5F5),
-                textStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+            const SizedBox(height: 15),
+            ElevatedButton(
+                      onPressed: () {
+                        _submit();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF1E3984),
+                        foregroundColor: Colors.white,
+                        textStyle: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        shadowColor: Colors.black.withAlpha(128),
+                        elevation: 10,
+                      ),
+                      child: Text('Iniciar sesión'),
+                    ),
+            const SizedBox(height: 10),
+            const Divider(),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Aun no eres usuario?', style: TextStyle(fontSize: 14)),
+                TextButton(
+                  onPressed: () {
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                    // );
+                  },
+                  child: const Text(
+                    'Registrate',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Text('Ingresar'),
+              ],
             ),
-          ),
-        ],
-      ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(builder: (context) => const ResetPasswordScreen()),
+                // );
+              },
+              child: const Text('Olvidaste tu contraseña?'),
+            ),
+          ]
+        )
+      )
     );
   }
 }
