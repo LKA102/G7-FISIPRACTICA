ed3f4bf23bffd97cb8458612803a2d420dcbf193
Gerson Aznarán <gersonec12@gmail.com>
2025-02-06 20:30:50 -0500

    Add login http request

--- flutter_app/lib/services/user_services.dart
+++ flutter_app/lib/services/user_services.dart
@@ -1,21 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger();
 
 final dio = Dio();
 
 class UserServices {
   static Future<List<Map<String, String>>> getEstudiantes() async {
     try {
+      String? token = getToken();
       Response response = await dio.get(
         'http://localhost:3000/student',
         options: Options(
           headers: {
-            'Authorization':
-                'Bearer {}',
+            'Authorization': 'Bearer $token',
           },
         ),
       );
+      logger.d(response);
       List<Map<String, String>> estudiantes = [];
-      print(response);
       for (var estudiante in response.data) {
         estudiantes.add({
           'nombre': estudiante['code'],
@@ -23,11 +26,40 @@ class UserServices {
           'descripcion': 'Descripción de ${estudiante['code']}',
         });
       }
-
       return estudiantes;
     } catch (e) {
-      print(e);
+      logger.e(e);
       return [];
     }
+  }
+
+  static Future<Map<String, String>> login(
+      String email, String password) async {
+    try {
+      Response response = await dio.post(
+        'http://localhost:3000/auth/login',
+        data: {
+          'email': email,
+          'password': password,
+        },
+      );
+
+      return {
+        'token': response.data['token'],
+      };
+    } catch (e) {
+      logger.e(e);
+      return {};
+    }
+  }
+
+  static String? _token;
+
+  static void setToken(String token) {
+    _token = token;
+  }
+
+  static String? getToken() {
+    return _token;
   }
 }
