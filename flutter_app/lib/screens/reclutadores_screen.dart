fc8cfd016c1d7fdf81a81dbeb228c39d88ba7033
fabrizioca2 <fabriziocago@gmail.com>
2025-02-07 14:09:57 -0500

    act dev

--- flutter_app/lib/screens/reclutadores_screen.dart
+++ flutter_app/lib/screens/reclutadores_screen.dart
@@ -17,7 +17,7 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
     {'nombre': 'María García López', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Scotiabank', 'experiencia': '4', 'fecha': 'septiembre, 2024 - actualidad'},
     {'nombre': 'José Martínez Ríos', 'foto': 'assets/profile_picture.jpg', 'empresa': 'MiBanco', 'experiencia': '3', 'fecha': 'octubre, 2024 - actualidad'},
     {'nombre': 'Carlos Pérez Vargas', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Alicorp', 'experiencia': '5', 'fecha': 'noviembre, 2024 - actualidad'},
-    {'nombre': 'Lucía Sánchez Gómez', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Backus', 'experiencia': '2', 'fecha': 'diciembre, 2024 - actualidad'},
+    {'nombre': 'Andrea Sánchez Gómez', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Backus', 'experiencia': '2', 'fecha': 'diciembre, 2024 - actualidad'},
     {'nombre': 'Sofía Fernández López', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Cemex Perú', 'experiencia': '1', 'fecha': 'enero, 2025 - actualidad'},
     {'nombre': 'Jorge Ruiz Fernández', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Corporación Lindley', 'experiencia': '3', 'fecha': 'febrero, 2025 - actualidad'},
     {'nombre': 'Felipe Vega González', 'foto': 'assets/profile_picture.jpg', 'empresa': 'Ferreyros', 'experiencia': '4', 'fecha': 'marzo, 2025 - actualidad'},
@@ -97,75 +97,74 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
   }
 
   void _showReclutadorDialog(Map<String, String> reclutador) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return Dialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          width: 300, // Ajusta el ancho del diálogo
-          constraints: BoxConstraints(
-            maxHeight: 400, // Limita la altura máxima de la ventana emergente
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 40,
-                backgroundImage: AssetImage(reclutador['foto']!),
-              ),
-              SizedBox(height: 10),
-              Text(
-                reclutador['nombre']!,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text('- RR/HH de ${reclutador['empresa']}'),
-              Text('- ${reclutador['experiencia']} años de experiencia'),
-              Text('- ${reclutador['fecha']}'),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            width: 300, 
+            constraints: BoxConstraints(
+              maxHeight: 400, 
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(reclutador['foto']!),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  reclutador['nombre']!,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Text('- RR/HH de ${reclutador['empresa']}'),
+                Text('- ${reclutador['experiencia']} años de experiencia'),
+                Text('- ${reclutador['fecha']}'),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text('Editar'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        // Agregar funcionalidad de edición si es necesario
+                      },
                     ),
-                    child: Text('Editar'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      // Agregar funcionalidad de edición si es necesario
-                    },
-                  ),
-                  SizedBox(width: 10),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
+                    SizedBox(width: 10),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text('Eliminar'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        _showConfirmationDialog(reclutador);
+                      },
                     ),
-                    child: Text('Eliminar'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      _showConfirmationDialog(reclutador);
-                    },
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      );
-    },
-  );
-}
+        );
+      },
+    );
+  }
 
-  // Función de confirmación de eliminación
   void _showConfirmationDialog(Map<String, String> reclutador) {
     showDialog(
       context: context,
@@ -185,6 +184,33 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
               onPressed: () {
                 Navigator.of(context).pop();
                 // Lógica para eliminar al reclutador
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showCreateReclutadorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Crear Nuevo Reclutador'),
+          content: Text('¿Deseas crear un nuevo reclutador?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Denegar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Confirmar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Aquí puedes agregar la lógica para crear un nuevo reclutador
               },
             ),
           ],
@@ -227,7 +253,6 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
               controller: _scrollController,
               itemCount: currentReclutadores.length,
               itemBuilder: (context, index) {
-                // Definir el color del contenedor según la empresa
                 String empresa = currentReclutadores[index]['empresa']!;
                 Color empresaColor;
 
@@ -291,7 +316,7 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
                       child: Center(
                         child: ListTile(
                           leading: Padding(
-                            padding: const EdgeInsets.only(right: 10), // Ajusta el espaciado entre la foto y el texto
+                            padding: const EdgeInsets.only(right: 10),
                             child: CircleAvatar(
                               backgroundImage: AssetImage(currentReclutadores[index]['foto']!),
                             ),
@@ -307,7 +332,7 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
                                   color: Color(0xFF1E3984),
                                 ),
                               ),
-                              SizedBox(height: 5),  // Ajusta el espaciado entre el nombre y la empresa
+                              SizedBox(height: 5),
                               Container(
                                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
                                 decoration: BoxDecoration(
@@ -353,6 +378,15 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
           Footer(),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 120), // Ajusta para subir el botón
+        child: FloatingActionButton(
+          onPressed: _showCreateReclutadorDialog,
+          child: Icon(Icons.add),
+          backgroundColor: Color(0xFF1E3984),
+        ),
+      ),
     );
   }
 
@@ -362,3 +396,4 @@ class _ReclutadoresScreenState extends State<ReclutadoresScreen> {
     super.dispose();
   }
 }
+
