49351f3fbe78d98be250bb41b9ae2b6629e5591b
Rosmeri Ccanto Flores <rosmeri.ccanto@unmsm.edu.pe>
2025-02-08 08:17:48 -0500

    añadiendo AppBar

--- flutter_app/lib/screens/home_estudiante.dart
+++ flutter_app/lib/screens/home_estudiante.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class HomeEstudiante extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Home Estudiante'),
+      backgroundColor: Colors.blueAccent,
+    ));
+  }
+}
