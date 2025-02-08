303a7022490d6e97bf745cc5dd39aa3b3c82e47c
JeanPier221 <96156595+JeanPier221@users.noreply.github.com>
2025-02-08 01:37:00 -0500

    Subiendo el proyecto flutter_app

---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import '../screens/home_admin_screen.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _email;
+  late TextEditingController _password;
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Email: $_email, Password: $_password');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    _email = TextEditingController(text: "");
+    _password = TextEditingController(text: "");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(30),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/logo_azul.png',
+              height: 208,
+              width: 211,
+            ),
+            TextFormField(
+              controller: _email,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Ej: pepito@unmsm.edu.pe',
+                label: Text("Correo electrónico o telefono")
+              )
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _password,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Ej: 123456',
+                label: Text("Contraseña")
+              )
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+                      onPressed: () {
+                        _submit();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF1E3984),
+                        foregroundColor: Colors.white,
+                        textStyle: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        shadowColor: Colors.black.withAlpha(128),
+                        elevation: 10,
+                      ),
+                      child: Text('Iniciar sesión'),
+                    ),
+            const SizedBox(height: 10),
+            const Divider(),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Aun no eres usuario?', style: TextStyle(fontSize: 14)),
+                TextButton(
+                  onPressed: () {
+                    // Navigator.of(context).push(
+                    //   MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                    // );
+                  },
+                  child: const Text(
+                    'Registrate',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(builder: (context) => const ResetPasswordScreen()),
+                // );
+              },
+              child: const Text('Olvidaste tu contraseña?'),
+            ),
+          ]
+        )
+      )
+    );
+  }
+}
