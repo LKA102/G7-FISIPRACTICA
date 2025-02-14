d49cca9915bad837312611ff3ff4d8a3618d8435
Gerson Aznarán <gersonec12@gmail.com>
2025-02-13 21:44:09 -0500

    Update user login routes

--- flutter_app/lib/widgets/login_form.dart
+++ flutter_app/lib/widgets/login_form.dart
@@ -5,7 +5,8 @@ import '../screens/reinicio_contraseña_screen.dart';
 import '../screens/home_reclutador_screen.dart';
 
 class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
+  final int index;
+  const LoginForm(this.index, {super.key});
 
   @override
   State<StatefulWidget> createState() => _LoginFormState();
@@ -18,8 +19,8 @@ class _LoginFormState extends State<LoginForm> {
 
   void _submit() async {
     if (_formKey.currentState!.validate()) {
-      final response =
-          await UserServices.login(_email.text, _password.text, 'Estudiante');
+      final response = await UserServices.login(_email.text, _password.text,
+          widget.index == 0 ? 'Reclutador' : 'Estudiante');
       if (!mounted) return;
 
       if (response.containsKey('token')) {
@@ -28,7 +29,11 @@ class _LoginFormState extends State<LoginForm> {
         print('Email: $_email, Password: $_password');
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => HomeReclutadorScreen()),
+          MaterialPageRoute(
+            builder: (context) => widget.index == 0
+                ? HomeReclutadorScreen()
+                : Container(), // Cambiar a HomeEstudianteScreen() cuando este disponible :v
+          ),
         );
       } else {
         // Manejar error de inicio de sesión
@@ -89,63 +94,63 @@ class _LoginFormState extends State<LoginForm> {
             decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Ej: 123456',
-                label: Text("Contraseña")
+                label: Text("Contraseña")),
+          ),
+          const SizedBox(height: 15),
+          ElevatedButton(
+            onPressed: () {
+              _submit();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF1E3984),
+              foregroundColor: Colors.white,
+              textStyle: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {
-                _submit();
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF1E3984),
-                foregroundColor: Colors.white,
-                textStyle: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                shadowColor: Colors.black.withAlpha(128),
-                elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Text('Iniciar sesión'),
+              shadowColor: Colors.black.withAlpha(128),
+              elevation: 10,
             ),
-            const SizedBox(height: 10),
-            const Divider(),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Aun no eres usuario?', style: TextStyle(fontSize: 14)),
-                TextButton(
-                  onPressed: () {
-                    // Navegar a la pantalla de registro
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const RegisterScreen()),
-                    );
-                  },
-                  child: const Text(
-                    'Registrate',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
+            child: Text('Iniciar sesión'),
+          ),
+          const SizedBox(height: 10),
+          const Divider(),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Aun no eres usuario?',
+                  style: TextStyle(fontSize: 14)),
+              TextButton(
+                onPressed: () {
+                  // Navegar a la pantalla de registro
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const RegisterScreen()),
+                  );
+                },
+                child: const Text(
+                  'Registrate',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-             onPressed: () {
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          TextButton(
+            onPressed: () {
               Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const ResetPasswordScreen()),
-          );
-         },
-  child: const Text('Olvidaste tu contraseña?'),
-)
-          ]
-        ),
+                MaterialPageRoute(
+                    builder: (context) => const ResetPasswordScreen()),
+              );
+            },
+            child: const Text('Olvidaste tu contraseña?'),
+          )
+        ]),
       ),
     );
   }
 }
-
