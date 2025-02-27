59c0b49a68dfde782fc10f176b08960425aa1230
Gerson Aznarán <gersonec12@gmail.com>
2025-02-27 17:28:00 -0500

    Agregar funcionalidad para registrar empresas con soporte para subir fotos

--- flutter_app/lib/services/empresas_services.dart
+++ flutter_app/lib/services/empresas_services.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_app/services/user_services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -24,6 +27,11 @@ class EmpresaServices {
       print(response.data);
       if (response.data is List) {
         for (var empresa in response.data) {
+          Uint8List? foto;
+          if (empresa['photo'] != null) {
+            foto = Uint8List.fromList(
+                (empresa['photo']['data'] as List<dynamic>).cast<int>());
+          }
           empresas.add({
             'id': empresa['id'],
             'nombre': empresa['name'],
@@ -31,11 +39,54 @@ class EmpresaServices {
             'descripcion': empresa['description'],
             'website': empresa['website'],
             'location': empresa['location'],
+            'foto': foto,
           });
         }
       }
       return empresas;
     } catch (e) {
+      logger.e(e);
+      rethrow;
+    }
+  }
+
+  static Future<Map<String, dynamic>> registerEmpresa(body, File? photo) async {
+    try {
+      final name = body['name'];
+      final address = body['address'];
+      final description = body['description'];
+      final website = body['website'];
+      final location = body['location'];
+      String? token = await UserServices.getToken();
+      logger.e('Aasdfsdf ${photo}');
+      Response response = await dio.post(
+        '${dotenv.env['API_DOMAIN']}/company',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'multipart/form-data'
+          },
+        ),
+        data: FormData.fromMap({
+          'name': name,
+          'address': address,
+          'description': description,
+          'website': website,
+          'location': location,
+          'photo': photo != null
+              ? await MultipartFile.fromFile(photo.path,
+                  filename: photo.path.split('/').last)
+              : null,
+          // Add other fields as required
+        }),
+      );
+      logger.e("$response");
+      return response.data;
+    } /*  on DioException {
+      logger.e('dio exception');
+      rethrow;
+    }  */
+    catch (e) {
       logger.e(e);
       rethrow;
     }
