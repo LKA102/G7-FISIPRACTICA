a698a892ad7e79cf9c93052591829c1384ec4a5a
Gerson Aznarán <gersonec12@gmail.com>
2025-02-26 21:14:13 -0500

    Set initial home screen by session role

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/HomeEstudianteScreen.dart';
+import 'package:flutter_app/screens/home_reclutador_screen.dart';
 import 'package:flutter_app/screens/splah_screen.dart';
+import 'package:flutter_app/services/user_services.dart';
+import 'package:flutter_app/widgets/main_estudiantes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
@@ -18,8 +22,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3984)),
         useMaterial3: true,
       ),
-      home:
-          const SplahScreen(), //Lleva donde se define ese widget - primera pantalla en mostrarse
+      home: FutureBuilder(
+        future: UserServices.getUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return const Text('Error');
+          } else if (snapshot.hasData) {
+            final user = snapshot.data as Map<String, dynamic>;
+            if (user['role'] == 'Estudiante') {
+              return const MainEstudiantes();
+            } else if (user['role'] == 'Reclutador') {
+              return const HomeReclutadorScreen();
+            } else {
+              return const SplahScreen();
+            }
+          } else {
+            return const SplahScreen();
+          }
+        },
+      ), //Lleva donde se define ese widget - primera pantalla en mostrarse
     );
   }
 }
