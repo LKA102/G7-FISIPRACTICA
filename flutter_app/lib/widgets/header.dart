07ea46094b9bc913d9f479a598682a96967a2b6d
Gerson Aznarán <gersonec12@gmail.com>
2025-02-27 19:58:44 -0500

    Agregar funcionalidad para mostrar un diálogo de confirmación al cerrar sesión

--- flutter_app/lib/widgets/header.dart
+++ flutter_app/lib/widgets/header.dart
@@ -1,9 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/splah_screen.dart';
+import 'package:flutter_app/services/user_services.dart';
 import 'package:flutter_app/widgets/admin_login_form.dart';
 
 class Header extends StatelessWidget {
   final bool isHome;
   const Header({super.key, this.isHome = false});
+
+  Future<void> _logout() async {
+    try {
+      final token = await UserServices.getToken();
+      final user = await UserServices.getUser();
+      await UserServices.logout();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            content: SizedBox(
+              width: 150,
+              height: 120,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Seguro que deseas cerrar sesión?',
+                    style: TextStyle(
+                      color: Color(0xFF1E3984),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF1E3984),
+                          foregroundColor: Colors.white,
+                        ),
+                        child: Text('Cerrar'),
+                        onPressed: () async {
+                          try {
+                            await _logout();
+                            if (context.mounted) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SplahScreen()),
+                              );
+                            }
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Color(0xFF1E3984),
+                          side: BorderSide(color: Color(0xFF1E3984)),
+                        ),
+                        child: Text('Cancelar'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
 
   void _showLoginDialog(BuildContext context) {
     showDialog(
@@ -80,7 +160,11 @@ class Header extends StatelessWidget {
               size: 40.0,
             ),
             onPressed: () {
-              if (!isHome) Navigator.pop(context);
+              if (!isHome) {
+                Navigator.pop(context);
+              } else {
+                _showLogoutDialog(context);
+              }
             },
           ),
         ));
